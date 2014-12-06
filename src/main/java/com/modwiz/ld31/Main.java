e4a9f7b0425d4e86e658efc274535117ec7440f1
Starbuck Johnson <starbuck@modwiz.com>
2014-12-05 21:31:01 -0600

    Added Gradle stuff

---
+++ src/main/java/com/modwiz/ld31/Main.java
@@ -0,0 +1,21 @@
+package com.modwiz.ld31;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+    }
+}
