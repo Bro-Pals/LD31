94614cb76c9ed009c2267ba789df7abb172424c4
Starbuck Johnson <starbuck@modwiz.com>
2014-12-05 22:59:53 -0600

    Forgot to uncomment

--- src/main/java/com/modwiz/ld31/Main.java
+++ src/main/java/com/modwiz/ld31/Main.java
@@ -1,5 +1,6 @@
 package com.modwiz.ld31;
 
+import com.modwiz.ld31.leveleditor.LevelEditorMain;
 import com.modwiz.ld31.world.GameObject;
 
 public class Main {
@@ -10,14 +11,6 @@ public class Main {
 			//The level editor is now OK
 		} else {
 			System.out.println("This shows that it is working!");
-			GameObject object = new GameObject(0, 0);
-			object.getVelocity().set(0, 20);
-			object.getVelocity().printVector();
 		}
-       System.out.println("This shows that it is working!");
-        /*GameObject object = new GameObject();
-        object.getVelocity().set(0, 20);
-        object.getVelocity().printVector();
-        */
     }
 }
