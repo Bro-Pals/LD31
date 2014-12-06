2498a4dd404a742039e689b121121e4c4d74ef4d
RadioLemon <horsentp@gmail.com>
2014-12-06 16:13:22 -0600

    Dragging?

--- src/main/java/com/modwiz/ld31/leveleditor/Cursor2D.java
+++ src/main/java/com/modwiz/ld31/leveleditor/Cursor2D.java
@@ -1,6 +1,7 @@
 package com.modwiz.ld31.leveleditor;
 
 import com.modwiz.ld31.entities.GameObject;
+import com.modwiz.ld31.entities.GameBlock;
 
 public class Cursor2D {
 	
@@ -9,6 +10,7 @@ public class Cursor2D {
 	private float offsetFromObjectX;
 	private float offsetFromObjectY;
 	private GameObject dragging;
+	private DragType type;
 	
 	public Cursor2D() {
 		x = 0;
@@ -31,14 +33,50 @@ public class Cursor2D {
 	}
 	
 	public void updateDrag() {
-		dragging.setX(getX() - offsetFromObjectX);
-		dragging.setY(getY() - offsetFromObjectY);
+		switch(type) {
+			case MOVE:
+				dragging.setX(getX() - offsetFromObjectX);
+				dragging.setY(getY() - offsetFromObjectY);
+				break;
+			case RESIZE_LEFT:
+				
+				break;
+			case RESIZE_UP:
+				
+				break;
+			case RESIZE_RIGHT:
+				
+				break;
+			case RESIZE_DOWN:
+				
+				break;
+		}
 	}
 	
 	public void startDragging(GameObject dragging) {
 		this.dragging = dragging;
 		offsetFromObjectX = getX() - dragging.getX();
 		offsetFromObjectY = getY() - dragging.getY();
+		type = DragType.MOVE;
+		//Check to see
+		if (dragging instanceof GameBlock) {
+			GameBlock gb = (GameBlock)dragging;
+			int leftBounds = Math.abs((int)(getX() - gb.getX()));
+			int rightBounds = Math.abs((int)(getX() - (gb.getX()+gb.getWidth())));
+			
+			int topBounds = Math.abs((int)(getY() - gb.getY()));
+			int bottomBounds = Math.abs((int)(getY() - (gb.getY()+gb.getHeight())));
+			
+			if (topBounds < 3) {
+				type = DragType.RESIZE_UP;
+			} else if (bottomBounds < 3) {
+				type = DragType.RESIZE_DOWN;
+			} else if (leftBounds < 3) {
+				type = DragType.RESIZE_LEFT;
+			} else if (rightBounds < 3) {
+				type = DragType.RESIZE_RIGHT;
+			}
+		}
 	}
 	
 	public void endDrag() {
