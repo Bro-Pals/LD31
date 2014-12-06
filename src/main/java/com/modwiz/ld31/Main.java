e7ffb73b215b56d4801eae5c9887ce93dd460b7d
Kevin <kprehn01@gmail.com>
2014-12-06 00:36:23 -0600

    No more build errors

--- src/main/java/com/modwiz/ld31/Main.java
+++ src/main/java/com/modwiz/ld31/Main.java
@@ -1,9 +1,11 @@
 package com.modwiz.ld31;
 
 import com.modwiz.ld31.leveleditor.LevelEditorMain;
+import com.modwiz.ld31.utils.assets.*;
 import com.modwiz.ld31.world.*;
 import horsentp.simpledrawing.DrawWindow;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 public class Main {
 
@@ -14,6 +16,23 @@ public class Main {
 		} else {
 			System.out.println("This shows that it is working!");
 			DrawWindow window = new DrawWindow("Super cool assasian game with next gen graphics", 800, 600, false);
+			
+			// loading all the animations
+			Resource playerMoving = null;
+			try {
+				playerMoving = AssetLoader.getAssetLoader().getResource("assets/img/playerMove.png").get();
+			
+			} catch(NullPointerException npe) {
+				System.out.println("Oh no it's a " + npe.toString());
+			} catch(Exception e) {
+				System.out.println("Problem loading images " + e.toString());
+				System.exit(0);
+			}
+			
+			BufferedImage[][] playerAnimations = new BufferedImage[4][];
+			for (int i=0; i<3; i++) {
+				playerAnimations[0][i] = ((BufferedImage)playerMoving.getContent()).getSubimage(i * 80, 0, 80, 120);
+			}
 			
 			Dimension firstDimension = new Dimension();
 			firstDimension.getObjects().add(new GameBlock(firstDimension, 50, 50, 100, 100)); // our first block!!
