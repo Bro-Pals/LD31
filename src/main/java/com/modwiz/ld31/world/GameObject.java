2500a49ef45227d45f3e9cb37d4f7a2289a32da9
Kevin <kprehn01@gmail.com>
2014-12-05 21:57:10 -0600

    GameObject

---
+++ src/main/java/com/modwiz/ld31/world/GameObject.java
@@ -0,0 +1,35 @@
+package com.modwiz.ld31.world;
+
+import lib.horsentp_math_2014.Vector2;
+
+/**
+	The extreme game object for everything that is going to go in the world
+*/
+public class GameObject {
+	
+	float x, y;
+	Vector2 velocity;
+	
+	public GameObject(float x, float y) {
+		this.x = x;
+		this.y = y;
+		velocity = new Vector2();
+	}
+	
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public void setLocaton(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+}
