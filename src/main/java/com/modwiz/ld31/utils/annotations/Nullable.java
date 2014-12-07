ec390e1a91f88fa876001de7630f981539b1056c
Starbuck Johnson <starbuck@modwiz.com>
2014-12-06 22:34:39 -0600

    This commit makes me sad :(

---
+++ src/main/java/com/modwiz/ld31/utils/annotations/Nullable.java
@@ -0,0 +1,8 @@
+package com.modwiz.ld31.utils.annotations;
+
+/**
+ * Indicates that it could be null
+ * Not enforced
+ */
+public @interface Nullable {
+}
--- src/main/java/com/modwiz/ld31/utils/assets/AssetLoader.java
+++ src/main/java/com/modwiz/ld31/utils/assets/AssetLoader.java
@@ -1,13 +1,5 @@
 package com.modwiz.ld31.utils.assets;
 
-import com.google.common.base.Optional;
-import com.modwiz.ld31.entities.GameObject;
-import com.modwiz.ld31.world.Dimension;
-import com.modwiz.ld31.world.GameWorld;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-
 public abstract class AssetLoader {
     /**
      * Returns the current implementation of AssetLoader
@@ -15,55 +7,7 @@ public abstract class AssetLoader {
      */
     public static AssetLoader getAssetLoader() {
         return new CachedLoader();
-    }
 
-    /**
-     * Load a generic resource object
-     * @param resourcePath The path to the generic resource object
-     * @return
-     */
-    public abstract <T> Optional<Resource<T>> getResource(String resourcePath, Class<T> resourceType);
-
-    /**
-     * Load a BufferedImage
-     * @param imagePath The path to the image file
-     * @return A buffered image with the image loaded
-     */
-    public abstract Optional<Resource<BufferedImage>> getBufferedImage(String imagePath);
-
-    /**
-     * Load a level from specified path
-     * @param levelPath The path to the level
-     * @return An object representing a game level
-     */
-    public abstract Optional<Resource<GameWorld>> getLevel(String levelPath);
-	
-	 /**
-     * Load a level from specific file; for the level editor
-     * @param levelFile The file that holds the level data
-     * @return An object representing a game level
-     */
-    public abstract Optional<Resource<GameWorld>> getLevelFromFile(File levelFile);
-	
-	 /**
-     * Load a level from specific file; for the level editor
-     * @param levelFile The file that holds the level data
-     * @return An object representing a game level
-     */
-    public abstract void saveLevel(File levelFile, GameWorld world);
-	
-	/**
-	* Creates a parse string for saving a GameObject.
-	* @param object The object to be converted into a String.
-	* @return The object in String form.
-	*/
-	public abstract String writeGameObject(GameObject object);
-	
-	/**
-	* Creates a GameObject from a parse string; reverse of writeGameObject.
-	* @param string The string to be converted into a GameObject.
-	* @return The String in object form.
-	*/
-	public abstract GameObject readGameObject(Dimension parent, String string);
+}
 
 }
