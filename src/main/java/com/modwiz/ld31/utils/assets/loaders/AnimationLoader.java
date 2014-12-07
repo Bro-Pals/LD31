64548f3dd3c43fc143d05aef4e272872f5919e8c
Kevin <kprehn01@gmail.com>
2014-12-07 00:04:29 -0600

    dsfasfsd

--- src/main/java/com/modwiz/ld31/utils/assets/loaders/AnimationLoader.java
+++ src/main/java/com/modwiz/ld31/utils/assets/loaders/AnimationLoader.java
@@ -12,8 +12,10 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
 
 /**
  * The class for loading {@link com.modwiz.ld31.entities.draw.Animation} class through
@@ -35,6 +37,14 @@ public class AnimationLoader implements ILoader<Animation> {
                 @Nullable
                 public Animation call() throws Exception {
                     Animation anim = null;
+					BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+					ArrayList<Animation> list = new ArrayList<>();
+					String nextLine;
+					while((nextLine = reader.readLine()) != null) {
+						if (!nextLine.startsWith("#")) { // otherwise a comment
+							
+						}
+					}
                     return anim;
                 }
             });
