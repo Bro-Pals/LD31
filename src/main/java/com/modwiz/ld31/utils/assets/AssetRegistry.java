8c5727146349490cb6358169fd159347b351aa68
Starbuck Johnson <starbuck@modwiz.com>
2014-12-06 19:29:22 -0600

    Added docs to AssetRegistry

--- src/main/java/com/modwiz/ld31/utils/assets/AssetRegistry.java
+++ src/main/java/com/modwiz/ld31/utils/assets/AssetRegistry.java
@@ -14,8 +14,17 @@ import java.util.Map;
 public class AssetRegistry<T> {
     private Map<String, Resource<T>> map = new HashMap<>();
 
+    /**
+     * The image texture registry, should be able to be shared.
+     */
     public static AssetRegistry<BufferedImage> bufferedImageRegistry = new AssetRegistry<>();
 
+    /**
+     * Registers an asset into the asset loader
+     * @param assetName The name of the asset in the loader
+     * @param asset The {@link com.modwiz.ld31.utils.assets.Resource} storing the asset
+     * @return Whether or not the registration was successful
+     */
     public boolean registerAsset(String assetName, Resource<T> asset) {
         if (map.containsKey(assetName)) {
             return false; // Existing asset with that name.
@@ -28,6 +37,11 @@ public class AssetRegistry<T> {
     }
 
 
+    /**
+     * Gets the content stored in an asset directly
+     * @param assetName The name of the asset
+     * @return The {@link T} loaded and stored in the asset
+     */
     public Optional<T> getContent(String assetName) {
         if (!map.containsKey(assetName)) {
             return Optional.absent();
@@ -35,6 +49,11 @@ public class AssetRegistry<T> {
         return Optional.of(map.get(assetName).getContent());
     }
 
+    /**
+     * Gets the path of an asset directly
+     * @param assetName The name of the asset
+     * @return The path to the asset's content
+     */
     public Optional<String> getPath(String assetName) {
         if (!map.containsKey(assetName)) {
             return Optional.absent();
@@ -42,6 +61,11 @@ public class AssetRegistry<T> {
         return Optional.of(map.get(assetName).getPath());
     }
 
+    /**
+     * Gets an asset from the asset registry
+     * @param assetName The name of the asset
+     * @return A {@link com.modwiz.ld31.utils.assets.Resource} asset
+     */
     public Optional<Resource<T>> getAsset(String assetName) {
         if (!map.containsKey(assetName)) {
             return Optional.absent();
@@ -49,6 +73,11 @@ public class AssetRegistry<T> {
         return Optional.of(map.get(assetName));
     }
 
+    /**
+     * Checks if an asset by the specified name exists
+     * @param assetName The name of the asset
+     * @return Whether or not the asset exists
+     */
     public boolean assetExists(String assetName) {
         return map.containsKey(assetName);
     }
